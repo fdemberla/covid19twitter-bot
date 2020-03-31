54ae250b13ed793e2b4c11b2b7b29c7afcac1952
Dember <Fernando_Dember@dell.com>
2020-03-31 11:17:29 -0500

    first commit

--- casos_por_genero.py
+++ casos_por_genero.py
@@ -5,18 +5,18 @@ def obtener_casos_por_genero():
     print("Extrayendo casos por genero...")
     pagina = open("./output/casos_por_genero.html")
     with pagina as markup:
-        soup = BeautifulSoup(markup.read(), "html.parser")
+        sopa = BeautifulSoup(markup.read(), "html.parser")
 
         lista = []
 
-        for tag in soup.findAll("text"):
+        for tag in sopa.findAll("text"):
             lista.append(tag.text.strip())
 
         objeto = {
             "femenino": int(lista[0]),
             "masculino": int(lista[1])
         }
-        
+
     print("Casos por genero extraidos!")
 
     return f'''Casos por genero:
